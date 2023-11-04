5751a88ad624851621658af3780cfbbe2c80124e
msilvasaquiray@gmail.com <msilvasaquiray@gmail.com>
2023-11-04 12:16:50 -0500

    update04

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,4 +1,5 @@
-const { getAll, getOne, create, remove, update, login, logged } = require('../controllers/user.controllers');
+const { getAll, getOne, update, login, logged } = require('../controllers/user.controllers');
+const { create, remove } = require('../controllers/user.controllers');
 
 const express = require('express');
 const { verifyJwt } = require('../utils/verifyJwt')
